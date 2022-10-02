2cfa6b82670bbc2dd570053a0ccf307c3772f633
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-02 09:35:30 -0400

    Removed unnecessary files created from create-react-app

--- server/db/migrate/20221001164746_create_reviews.rb
+++ server/db/migrate/20221001164746_create_reviews.rb
@@ -1,4 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :body
+      t.string :name
+      t.integer :book_id
+      t.float :rating
+      t.timestamps
   end
 end
