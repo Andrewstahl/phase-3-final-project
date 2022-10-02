b1cd04a5bf0f1197546d9f7bafe63e50b0879357
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-02 10:13:25 -0400

    Working on adding seed data for the database

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,5 +1,22 @@
 puts "🌱 Seeding spices..."
 
+# t.string :title
+# t.string :genre
+# t.integer :author_id
+# t.string :read_status
+# t.date :finished_date
+# t.string :image_url
+
 # Seed your database here
+rdawkins = Author.create("Richard Dawkins")
+Book.create(
+  title: "The Selfish Gene"
+  genre: "Science"  
+)
+
+# t.string :body
+# t.string :name
+# t.integer :book_id
+# t.float :rating
 
 puts "✅ Done seeding!"
