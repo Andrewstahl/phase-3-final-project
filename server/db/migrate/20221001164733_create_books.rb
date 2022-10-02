8236e8d887e7e05cd7a50a12c94ee701fa33470f
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-02 09:37:49 -0400

    Removed old files from create-react-app and updated migrations

--- server/db/migrate/20221001164733_create_books.rb
+++ server/db/migrate/20221001164733_create_books.rb
@@ -1,4 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
+    create_table :books do |t|
+      t.string :title
+      t.string :genre
+      t.integer :author_id
+      t.string :read_status
+      t.date :finished_date
+      t.timestamps
+    end
   end
 end
