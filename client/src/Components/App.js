1359e9a9d32821167913e0c2df467d175ea446d7
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-03 20:34:55 -0400

    Added structure hierarchy for app and imported dependencies

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,4 +1,28 @@
-import './App.css';
+import  '../../src/App.css';
+import  '../../src/index.css';
+import React, { useEffect, useState } from 'react';
+import {Route, Routes, useNavigate } from 'react-router-dom';
+
+/**
+ * App Hierarchy
+ * 
+ * App
+ * ├─── Header
+ * ├─── NavBar
+ * ├─── Books
+ *      ├─── Book
+ *      ├─── Book
+ *      └─── Book
+ * ├─── Reviews
+ *      ├─── Review
+ *      ├─── Review
+ *      └─── Review
+ * └─── Authors
+ *      ├─── Author
+ *      ├─── Author
+ *      └─── Author
+ * 
+ */
 
 function App() {
   return (
