e3625775a0149b2df9c4068202145cc6f12f0cb4
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-04 10:50:00 -0400

    Code working for AuthorList and Author components

--- client/src/Components/Author.js
+++ client/src/Components/Author.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 export default function Author({ author }) {
+  const {id, name, books} = author
+  // console.log(books)
   return (
-    <>
-    </>
+    <div>
+      <h3>{name}</h3>
+      {books.map(book => {
+        return (
+          <span className="book-span">{book.title}</span>
+        )
+      })}
+    </div>
   )
 }
--- client/src/Components/AuthorList.js
+++ client/src/Components/AuthorList.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Author from './Author'
 
 export default function Authors() {
+  const [authors, setAuthors] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/authors")
+      .then(r => r.json())
+      .then(data => setAuthors(data))
+  }, [])
+
+  const authorElements = authors.map(author => {
+    return (
+      <div>
+        <Author key={author.id} author={author}/>
+      </div>
+    )
+  })
+  
   return (
-    <>
-    </>
+    <div>
+      {authorElements}
+    </div>
   )
 }
