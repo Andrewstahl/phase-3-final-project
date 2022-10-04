dd375892b0035132a0666edbf857115f9e71c439
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-04 11:04:58 -0400

    Code working for ReviewList and Review components

--- client/src/Components/Review.js
+++ client/src/Components/Review.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 export default function Review({ review }) {
+  const {id, body, rating, book} = review
+
   return (
-    <>
-    </>
+    <div>
+      <h4>{book.title}</h4>
+      <span>{rating}</span>
+      <p>{body}</p>
+    </div>
   )
 }
--- client/src/Components/ReviewList.js
+++ client/src/Components/ReviewList.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Review from "./Review";
 
 export default function Reviews() {
+  const [reviews, setReviews] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/reviews")
+      .then(r => r.json())
+      .then(data => setReviews(data))
+  }, [])
+
+  const reviewElements = reviews.map(review => {
+    return (
+      <div>
+        <Review key={review.id} review={review}/>
+      </div>
+    )
+  })
+  
   return (
-    <>
-    </>
+    <div>
+      {reviewElements}
+    </div>
   )
 }
