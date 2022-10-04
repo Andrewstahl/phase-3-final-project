a3bc369c974ca50a9b8ece99b02c312ce9a0bd11
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-04 17:59:46 -0400

    Added navlinks and styles

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -28,7 +28,45 @@ const navLinkStyles = {
 
 export default function NavBar() {
   return (
-    <>
-    </>
+    <div style={{ margin: "0 200px" }}>
+      <div style={{ ...navLinkStyles }}>
+        <NavLink 
+          to="/"
+          exact
+          style={({ isActive }) => (
+            isActive ? linkStylesActive : linkStyles 
+          )}
+        >
+          Home
+        </NavLink>
+        <NavLink 
+          to="/books"
+          exact
+          style={({ isActive }) => (
+            isActive ? linkStylesActive : linkStyles 
+          )}
+        >
+          Books
+        </NavLink>
+        <NavLink 
+          to="/reviews"
+          exact
+          style={({ isActive }) => (
+            isActive ? linkStylesActive : linkStyles 
+          )}
+        >
+          Reviews
+        </NavLink>
+        <NavLink 
+          to="/authors"
+          exact
+          style={({ isActive }) => (
+            isActive ? linkStylesActive : linkStyles 
+          )}
+        >
+          Authors
+        </NavLink>
+      </div>
+    </div>
   )
 }
