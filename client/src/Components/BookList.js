331fb191329f586dd8a51f280979d29e484997b8
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-03 21:22:14 -0400

    Troubleshooting Book List

--- client/src/Components/BookList.js
+++ client/src/Components/BookList.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Book from "./Book";
 
 export default function BookList() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/books")
+      .then(r => r.json())
+      .then(data => console.log(data))
+  }, [])
+
+  const bookElements = books.map(book => {
+    return (
+      <div className="book-element">
+        <Book key={book.id} book={book} />
+      </div>
+    )
+  })
+  
   return (
-    <>
-    </>
+    <div>
+      {bookElements}
+    </div>
   )
 }
