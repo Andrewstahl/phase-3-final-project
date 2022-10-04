aaabb852f85d51098aace08291d229cd087868a4
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-04 11:04:27 -0400

    Removed unnecessary code

--- client/src/Components/Book.js
+++ client/src/Components/Book.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 
 export default function Book({ book }) {
-  // const bookData = book
-  const {id, title, read_status, finished_date, image_url, genres} = book;
+  const {id, title, author, read_status, finished_date, image_url, genres} = book;
   
   return (
     <div>
-      <h3>{book.title}</h3>
-      <h4>{title}</h4>
+      <h3>{title}</h3>
+      <h4>{author.name}</h4>
       <img alt={`${title} book cover`} src={image_url}></img>
       <p>{read_status}</p>
       <p>{finished_date}</p>
-      <p>{genres}</p>
+      {genres.map(genre => {
+        return (
+          <span className="genre-span">{genre}</span>
+        )
+      })}
     </div>
   )
 }
