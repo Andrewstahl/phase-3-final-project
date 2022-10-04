b62afbdc2006aa482231f1bce994a9f09d1cd338
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-03 20:44:13 -0400

    Created a home component as the base

---
+++ client/src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <h2>Welcome to the Stahl Book Tracker!</h2>
+      <p>This project houses a compilation of books, authors, and reviews. 
+        From here, you can keep track of books that you have read or want to read,
+        authors that you're interested in, and reviews for the books you have completed.
+      </p>
+      <p>
+        To get started, please select something in the navbar.
+      </p>
+    </>
+  )
+}
