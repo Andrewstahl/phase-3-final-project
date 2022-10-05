49b9085e4300fdf1b700e45cfeb7bdda4ce73749
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-05 18:08:19 -0400

    Changed name of default export function to match naming conventions for AuthorList and ReviewList

--- client/src/Components/BookList.js
+++ client/src/Components/BookList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Book from "./Book";
 
-export default function BookList() {
+export default function Books() {
   const [books, setBooks] = useState([])
 
   useEffect(() => {
