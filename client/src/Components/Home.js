77dd3622dd7027f3a4fbac0deb912b514b2856fd
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-05 05:43:20 -0400

    Added class names for title and body for more effective formatting

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Home() {
   return (
-    <>
-      <h2>Welcome to the Stahl Book Tracker!</h2>
+    <div className="home-page-div">
+      <h2 className="home-page-header">Welcome to the Stahl Book Tracker!</h2>
       <p>This project houses a compilation of books, authors, and reviews. 
         From here, you can keep track of books that you have read or want to read,
         authors that you're interested in, and reviews for the books you have completed.
@@ -11,6 +11,6 @@ export default function Home() {
       <p>
         To get started, please select something in the navbar.
       </p>
-    </>
+    </div>
   )
 }
