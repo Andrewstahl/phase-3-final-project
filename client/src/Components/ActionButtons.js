180b3aec14ea244e6005071ee68f193a7bd6bd6b
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-05 07:24:57 -0400

    Added component to handle the CRUD action buttons for all applicable areas

---
+++ client/src/Components/ActionButtons.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ActionButtons() {
+  return (
+    <div className="card-action-buttons">
+      <button className="card-action-button">Edit</button>
+      <button className="card-action-button">Delete</button>
+    </div>
+  )
+}
