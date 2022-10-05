7643510663256547145fdd55341b4621f48c89a2
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-05 19:11:47 -0400

    Adding functions for editing/deleting reviews

--- client/src/Components/Review.js
+++ client/src/Components/Review.js
@@ -1,15 +1,31 @@
 import React from "react";
 import ActionButtons from './ActionButtons'
 
-export default function Review({ review }) {
+export default function Review({ review, handleEdit, onDelete }) {
   const {id, body, rating, book} = review
+
+  function handleEdit() {
+    
+  }
+  
+  function handleDelete() {
+    fetch(`http://localhost:9292/reviews/${id}`, {
+      method: "DELETE", 
+      headers: {
+        "CONTENT-TYPE": "application/json"
+      },
+      body: JSON.stringify({})
+    })
+      .then(r => r.json())
+      .then(data => onDelete(data))
+  }
 
   return (
     <div className="review-on-book-details">
       <h4>{book.title}</h4>
       <span>Rating: {rating}</span>
       <p>{body}</p>
-      <ActionButtons />
+      <ActionButtons onEdit={handleEdit} onDelete={handleDelete}/>
     </div>
 
   )
