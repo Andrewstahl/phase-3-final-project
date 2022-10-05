c1a783b00b8c587b30275fb376049654a1d955d0
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-05 18:06:55 -0400

    Added callback functions for edit and delete

--- client/src/Components/ActionButtons.js
+++ client/src/Components/ActionButtons.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-export default function ActionButtons() {
+export default function ActionButtons(onEdit, onDelete) {
   return (
     <div className="card-action-buttons">
-      <button className="card-action-button">Edit</button>
-      <button className="card-action-button">Delete</button>
+      <button 
+        className="card-action-button"
+        onClick={() => onEdit}
+        >
+        Edit
+      </button>
+      <button 
+        className="card-action-button"
+        onClick={() => onDelete}
+      >
+        Delete
+      </button>
     </div>
   )
 }
