142ef15140ec12a96625212d223c0d86f5bd9b4d
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-07 09:29:05 -0400

    Added separate component for adding a new review

---
+++ client/src/Components/NewReview.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function NewReview({ onSubmit }) {
+  const params = useParams();
+  const [rating, setRating] = useState(0.0)
+  const [textarea, setTextarea] = useState("")
+
+  function handleChange(e) {
+    const name = e.target.name;
+    // The rating comes over as a string
+    if (name === "rating") {
+      setRating(parseFloat(e.target.value))
+    }
+    else if (name === "body") {
+      setTextarea(e.target.value)
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({
+      book_id: parseInt(params.id),
+      body: textarea,
+      rating: rating
+    })
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="rating">Review Rating</label>
+        <input
+          type="number"
+          name="rating"
+          id="rating"
+          step="0.01"
+          value={rating}
+          onChange={(e) => handleChange(e)}
+        />
+        <label htmlFor="body">Review Body</label>
+        <textarea id="body" name="body" value={textarea} onChange={(e) => handleChange(e)}/>
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  )
+
+}
