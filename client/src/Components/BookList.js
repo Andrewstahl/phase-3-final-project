fd1ffbedcf588aa7d6960b51c10a56c3d5f0f2d0
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-10 15:24:06 -0400

    Added function for handling adding a new book

--- client/src/Components/BookList.js
+++ client/src/Components/BookList.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Book from "./Book";
 import AddEditBook from "./AddEditBook";
+import { redirect } from "react-router-dom";
 
-export default function Books() {
+export default function BookList() {
   const [books, setBooks] = useState([])
   const [showAddBook, setShowAddBook] = useState(false)
 
@@ -19,11 +20,25 @@ export default function Books() {
       </div>
     )
   })
+
+  function handleAddBook(addedBook) {
+    fetch(`http://localhost:9292/books`, {
+      method: "POST",
+      headers: {
+        "CONTENT-TYPE": "application/json"
+      },
+      body: JSON.stringify(addedBook)
+    })
+    .then(r => r.json())
+    .then(data => console.log(data))
+
+    redirect("/books")
+  }
   
   return (
     <>
       {showAddBook ?
-        <AddEditBook currentBook={undefined} />
+        <AddEditBook currentBook={undefined} fetchMethod={"POST"} />
         :
         null
       }
