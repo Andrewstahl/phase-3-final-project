94d9bd1f42b5346c9f4eff2b2e68b8f9eeee7bfc
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-10 16:43:52 -0400

    Removed extraneous code

--- client/src/Components/Review.js
+++ client/src/Components/Review.js
@@ -3,24 +3,6 @@ import React from "react";
 export default function Review({ review, book, onEdit, onDelete }) {
   const {id, body, rating} = review
 
-  function handleEdit() {
-    console.log("Review.js I'm being edited", id)
-    
-  }
-  
-  function handleDelete() {
-    console.log("Review.js I'm being deleted", id)
-    // fetch(`http://localhost:9292/reviews/${id}`, {
-    //   method: "DELETE", 
-    //   headers: {
-    //     "CONTENT-TYPE": "application/json"
-    //   },
-    //   body: JSON.stringify({})
-    // })
-    //   .then(r => r.json())
-    //   .then(data => onDelete(data))
-  }
-
   return (
     <div className="review-on-book-details">
       {book != null 
@@ -39,6 +21,5 @@ export default function Review({ review, book, onEdit, onDelete }) {
         </button>
       </div>
     </div>
-
   )
 }
