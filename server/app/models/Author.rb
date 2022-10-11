942fb59ca7daabc9a9f4994e7463f52bf5424967
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-11 19:00:17 -0400

    Added method for pulling in authors with empty books

--- server/app/models/Author.rb
+++ server/app/models/Author.rb
@@ -1,4 +1,15 @@
 class Author < ActiveRecord::Base
   has_many :books
   has_many :reviews, through: :books
+
+  def self.empty_books
+    empty_books_array = []
+    self.all.each do |author|
+      if author.books.count == 0
+        empty_books_array << author
+      end
+    end
+    empty_books_array
+  end
+
 end
