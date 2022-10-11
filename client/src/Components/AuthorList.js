870f26620aa4baf07b5eabfd8814d2c391d3d2d5
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-10 21:41:04 -0400

    Major functions added for CRUD actions. Also changed formatting and syntax structure

--- client/src/Components/AuthorList.js
+++ client/src/Components/AuthorList.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import AddEditAuthor from "./AddEditAuthor";
 import Author from './Author'
 
-export default function Authors() {
+export default function AuthorList() {
   const [authors, setAuthors] = useState([])
+  const [currentAuthor, setCurrentAuthor] = useState()
+  const [showEditAuthor, setShowEditAuthor] = useState(false)
 
   useEffect(() => {
     fetch("http://localhost:9292/authors")
@@ -10,17 +13,64 @@ export default function Authors() {
       .then(data => setAuthors(data))
   }, [])
 
+  function handleEditClick(author) {
+    setCurrentAuthor(author)
+    setShowEditAuthor(!showEditAuthor)
+  }
+  
+  function handleEditSubmit(editedAuthor) {
+    const updatedAuthors = authors.map(author => {
+      if (author.id === editedAuthor.id) {
+        return editedAuthor
+      } else {
+        return author
+      }
+    })
+    setAuthors(updatedAuthors)
+    setShowEditAuthor(false)
+  }
+  
+  function handleDelete(deletedAuthor) {
+    fetch(`http://localhost:9292/authors/${deletedAuthor.id}`, {
+      method: "DELETE",
+      headers: {
+        "CONTENT-TYPE": "application/json"
+      }
+    })
+    .then(r => r.json())
+    .then(data => {
+      const updatedAuthors = authors.filter(author => author.id !== deletedAuthor.id)
+      setAuthors(updatedAuthors)
+    })
+  }
+  
   const authorElements = authors.map(author => {
     return (
       <div>
-        <Author key={author.id} author={author}/>
+        <Author 
+          key={author.id} 
+          author={author} 
+          onEdit={handleEditClick} 
+          onDelete={handleDelete}
+        />
       </div>
     )
   })
   
   return (
-    <div>
-      {authorElements}
-    </div>
+    <>
+      {showEditAuthor 
+        ? <AddEditAuthor 
+            currentAuthor={currentAuthor}
+            fetchMethod={"PATCH"}
+            onSubmit={handleEditSubmit}
+            onCancel={() => setShowEditAuthor(!showEditAuthor)}
+          />
+        : null
+      }
+      <div>
+        {authorElements}
+      </div>
+    </>
   )
 }
